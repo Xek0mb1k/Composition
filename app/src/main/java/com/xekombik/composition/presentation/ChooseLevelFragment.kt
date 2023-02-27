b50306e980f35ffd1e7ac5d958bc5d87f93a5111
Xek0mb1k <oganes969@gmail.com>
2023-02-27 20:24:41 +0300

    Add view binding

--- app/src/main/java/com/xekombik/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/xekombik/composition/presentation/ChooseLevelFragment.kt
@@ -6,13 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.xekombik.composition.R
+import com.xekombik.composition.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
+    private var _binding : FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.testLevelButton.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/xekombik/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/xekombik/composition/presentation/GameFinishedFragment.kt
@@ -6,13 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.xekombik.composition.R
+import com.xekombik.composition.databinding.FragmentGameBinding
+import com.xekombik.composition.databinding.FragmentGameFinishedBinding
 
 class GameFinishedFragment : Fragment() {
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get () = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tryAgainButton.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
